dab5d996cbb9c4d771c74cf50d407a8e221ff687
Sándor Bakos <bakos.webd@gmail.com>
2019-02-08 10:19:02 +0100

    opt1

--- Python/Class_openCV.py
+++ Python/Class_openCV.py
@@ -1,9 +1,7 @@
 # Setup
 
-import numpy as np
 import serial
 import time
-import sys
 import cv2
 
 
@@ -12,15 +10,10 @@ class CV:
     def __init__(self, port, baudrate):
         self.port = port
         self.baudrate = baudrate
-        self.value1 = 0
-        self.value2 = 0
+        self.arduino = None
         self.LEDIndex = 0
         self.imgIndex = 0
-        self.faceHeight = 0
-        self.faceWidth = 0
         self.faceInFrame = True
-        self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        self.cap = cv2.VideoCapture(0)
 
 
     def connect(self):
@@ -30,43 +23,35 @@ class CV:
 
 
     def LEDonoff(self):
-        if self.LEDIndex == 0:
-            self.value1 = 49
-            self.value2 = 13
-            self.arduino.write(bytearray([self.value1, self.value2]))
+        if self.LEDIndex == 0: # 0 is off
+            self.arduino.write(bytearray([49, 13]))
             self.LEDIndex = 1
             print("LED ON")
-        elif self.LEDIndex == 1:
-            self.value1 = 48
-            self.value2 = 13
+        elif self.LEDIndex == 1: # 1 is on
+            self.arduino.write(bytearray([48, 13]))
             self.LEDIndex = 0
-            self.arduino.write(bytearray([self.value1, self.value2]))
             print("LED OFF")
 
     def coordinates(self):
         cv2.rectangle(self.img, (self.x, self.y), (self.x + self.w, self.y + self.h), (255, 0, 0), 5)
         self.xx = int((self.x + (self.x + self.w)) / 6)
-        self.faceWidth = int(self.w)
         self.yy = int((self.y + (self.y + self.h)) / 6)
-        self.faceHeight = int(self.h)
 
         self.x = int(self.xx)
-        self.xcoordinate = 50
         self.y = int(self.yy)
-        self.ycoordinate = 51
-        CV.ArduinoSend(self)
+        self.ArduinoSend()
 
         center = (self.x, self.w, self.y, self.h)
         print("Center of Rectangle is :", center, '\n \n')
 
-        if self.faceInFrame == True and self.faceWidth <= 120 and self.faceHeight <= 120:
-            CV.takepicture(self)
+        if self.faceInFrame == True:
+            self.takepicture()
         time.sleep(0.01)
         cv2.imshow('img', self.img)
 
-    def ArduinoSend(self):
-        self.arduino.write(bytearray([self.xcoordinate, self.x]))
-        self.arduino.write(bytearray([self.ycoordinate, self.y]))
+    def ArduinoSend(self): # the arduino pin 2 and 3 configured this way to receive only 2 bytes
+        self.arduino.write(bytearray([50, self.x]))
+        self.arduino.write(bytearray([51, self.y]))
 
     def takepicture(self):
         self.imgIndex += 1
@@ -74,26 +59,29 @@ class CV:
         cv2.imwrite("Images\LatestImage" + str(self.imgIndex) + ".jpg", self.img)
 
     def detection(self):
-        ret, self.img = self.cap.read()
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        cap = cv2.VideoCapture(0)
+        ret, self.img = cap.read()
         gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
-        faces = self.face_cascade.detectMultiScale(gray, 1.3, 3)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 3)
         for (self.x, self.y, self.w, self.h) in faces:
-            CV.coordinates(self)
+            self.coordinates()
         cv2.imshow('img', self.img)
         if faces == ():
             self.faceInFrame = True
             print(self.faceInFrame)
 
+
     def loop(self):
         while 1:
-            images.detection()
+            self.detection()
             k = cv2.waitKey(30) & 0xff  # 3
             if k == 27:
                 break
             elif k == 111:  # if O(letter o not 0) is pressed the LED(pin 13) will be turned on or off depending on it's current state. The pin has to start as being LOW
-                images.LEDonoff()
+                self.LEDonoff()
 
 
-images = CV(input("Port: "), input("Baudrate: "))
+images = CV("COM4", 115200)
 images.connect()
 images.loop()
